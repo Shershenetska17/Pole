3b3656580831f65a9f395377ec751f4de60a4067
shers <shers@LIZA>
2024-04-25 10:58:32 +0200

    Add project files.

---
+++ Biblioteka/PoleFigury.cs
@@ -0,0 +1,20 @@
+﻿namespace Biblioteka
+{
+    public class PoleFigury
+    {
+        public static double Kwadrat(double a)
+        {
+            double pole;
+            pole = a * a;
+            return pole;
+        }
+
+        public static double Kolo(double r)
+        {
+            double pole;
+            pole = Math.PI * r * r;
+            return pole;
+        }
+
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj długość boku kwadratu: ");
+            double bok = Convert.ToDouble(Console.ReadLine());
+            double kw = Biblioteka.PoleFigury.Kwadrat(bok);
+            Console.WriteLine("Pole kwadratu - {0}", kw);
+
+            Console.Write("\nPodaj długość promienia: ");
+            double promien = Convert.ToDouble(Console.ReadLine());
+            double ko = Biblioteka.PoleFigury.Kolo(promien);
+            Console.WriteLine("Pole koła - {0}", ko);
+
+            Console.WriteLine("\nKoniec Programu");
+            Console.ReadKey();
+
+
+        }
+    }
+}
